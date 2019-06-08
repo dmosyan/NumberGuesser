7753c2c06d7c819b340f4e430e771779b20696ab
Hasmik <hasmiklorikyan@globanet.com>
2019-06-08 23:37:16 +0400

    1. Added option, when input is not a number 2. Changed fixed number to random number

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -31,7 +31,11 @@ namespace NumberGuesser
             Console.WriteLine("Hello {0}, let's play a game.", inputName);
 
             //Init correct number
-            int correctNumber = 7;
+            //int correctNumber = 7;
+            //Creating random number
+            Random random = new Random();
+            int correctNumber = random.Next(1, 10);
+
             //Init guess
             int guess = 0;
 
@@ -43,6 +47,14 @@ namespace NumberGuesser
             {
                 //Get user input
                 string input = Console.ReadLine();
+
+                //Make sure that it's number
+                if (!int.TryParse(input, out guess)) {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("It's not a number");
+                    Console.ResetColor();
+                    continue;
+                }
 
                 //Change to integer and put in guess
                 guess = Int32.Parse(input);
